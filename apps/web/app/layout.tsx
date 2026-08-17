b78d769209e5960af5b287f67ff609d190603734
Logesh <loganlogesh17@gmail.com>
2026-08-17 05:59:57 +0530

    Load credential upload styles

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { DM_Sans, Space_Grotesk } from "next/font/google";
 import "./styles.css";
+import "./credential-upload.css";
 
 const bodyFont = DM_Sans({ subsets: ["latin"], variable: "--font-body" });
 const displayFont = Space_Grotesk({ subsets: ["latin"], variable: "--font-display" });
