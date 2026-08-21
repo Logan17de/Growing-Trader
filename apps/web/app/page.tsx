3addc594332576cb2e925eabe342747abfc8bd81
Logesh <loganlogesh17@gmail.com>
2026-08-21 06:39:24 +0530

    Gate dashboard with email OTP

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,6 @@
 import SignalDashboard from "@/components/SignalDashboard";
+import { DashboardOtpGate } from "@/components/terminal/DashboardOtpGate";
 
 export default function Home() {
-  return <SignalDashboard />;
+  return <DashboardOtpGate><SignalDashboard /></DashboardOtpGate>;
 }
